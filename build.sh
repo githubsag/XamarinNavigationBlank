37c1b8108b55f0e286da184fc4703b9a71e019ad
githubsag <sohrabag@kth.se>
2024-08-19 17:40:52 +0200

    Update build.sh
    modified build.sh:
    added: restore XamarinNavigationBlank.sln

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /Library/Frameworks/Mono.framework/Versions/4.4.0/bin/mono $MONO_OPTIONS /Library/Frameworks/Mono.framework/Versions/4.4.0/lib/mono/nuget/NuGet.exe "$@"
+exec /Library/Frameworks/Mono.framework/Versions/Current/bin/mono $MONO_OPTIONS /Library/Frameworks/Mono.framework/Versions/Current/lib/mono/nuget/NuGet.exe restore XamarinNavigationBlank.sln "$@"

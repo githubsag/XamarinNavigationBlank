7d2867cf57e60e4779bf187222e528dc8768bca1
sohrab <azamighadim2@gmail.com>
2024-08-19 12:31:47 +0200

    added XamarinNavigationBlank to repository

---
+++ XamarinNavigationBlank/AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using XamarinNavigationBlank.ViewModels;
+using XamarinNavigationBlank.Views;
+using Xamarin.Forms;
+
+namespace XamarinNavigationBlank
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
